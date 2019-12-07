7e5e00666c506f4bf584a2fff1850ec1bb0e4e9f
gongmeng <18588264128@163.com>
2019-12-07 03:46:57 +0000

    找回密码成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -26,4 +27,17 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = '/';
+
+    /**
+     * 重写此方法
+     * [sendResetResponse 闪存提示信息并跳转到首页]
+     * @param  Request $request  [description]
+     * @param  [type]  $response [description]
+     * @return [type]            [description]
+     */
+    protected function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success', '密码更新成功，您已成功登录！');
+        return redirect($this->redirectPath());
+    }
 }
