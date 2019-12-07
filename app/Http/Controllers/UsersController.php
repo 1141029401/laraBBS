ccd2c99e39c02fa17083b98de202c941d236a532
gongmeng <18588264128@163.com>
2019-12-07 09:10:17 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,12 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
+
     //个人中心
     public function show(User $user){
 
@@ -18,12 +24,16 @@ class UsersController extends Controller
     //编辑资料
     public function edit(User $user){
 
+        $this->authorize('update', $user);
+
     	return view("users.edit",compact('user'));
     }
 
 
     public function update(UserRequest $request,  ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
+        
     	$data = $request->all();
 
         if ($request->avatar) {
