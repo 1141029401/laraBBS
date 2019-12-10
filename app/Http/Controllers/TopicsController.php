b4e02a5cafecb88688abd98e6e69597f4ef57594
gongmeng <18588264128@163.com>
2019-12-10 08:50:37 +0000

    用户可以编辑话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -49,12 +49,15 @@ class TopicsController extends Controller
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
 	}
 
+	//编辑帖子页面
 	public function edit(Topic $topic)
 	{
-        $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
+       $this->authorize('update', $topic);
+       $categories = Category::all();
+       return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
+	//处理编辑请求
 	public function update(TopicRequest $request, Topic $topic)
 	{
 		$this->authorize('update', $topic);
