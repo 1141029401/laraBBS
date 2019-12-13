0aca10e4db050e983ed70a764d5c8fc31f1b26b3
gongmeng <18588264128@163.com>
2019-12-13 02:06:11 +0000

    集成 laravel-permission 扩展包

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -42,12 +42,27 @@ class UsersTableSeeder extends Seeder
         // 插入到数据库中
         User::insert($user_array);
 
-        // 单独处理第一个用户的数据
+         // 单独处理第一个用户的数据
+        $user1 = User::find(1);
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user1->assignRole("Founder");
+        $user1->name = "萌萌哒";
+        $user1->email = "1141029401@qq.com";
+        $user1->password = bcrypt("12345678");
+        $user1->save();
+
+        // 单独处理第二个用户的数据
         $user = User::find(2);
+        // 将 2 号用户指派为『管理员』
+        $user->assignRole('Maintainer');
+
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->password = bcrypt("12345678"); 
         $user->save();
+
+
+
     }
 }
